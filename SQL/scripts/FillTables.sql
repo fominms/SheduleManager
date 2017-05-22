d127adb26c3381d66f6bd7335510f7b3254188ab
freakmiha <freakmiha@users.noreply.github.com>
2017-05-22 23:10:32 +0300

    Скрип для заполнения таблиц значениями

---
+++ SQL/scripts/FillTables.sql
@@ -0,0 +1,8 @@
+﻿-- {Заполнение таблиц}
+insert into dbo.t_Weeks (Name, Symbol) values ('Нечетная', 'I'), ('Четная','II')
+insert into dbo.t_Semestrs (Name) values ('Весенний'), ('Осенний')
+insert into dbo.t_Days (Name) values ('Понедельник'), ('Вторник'), ('Среда'), ('Четверг'), ('Пятница'), ('Суббота')
+insert into dbo.t_Lesson_Number (Number, TimeBegin, TimeEnd) values (1, '9:00', '10:30'), (2, '10:40', '12:10'), (3, '13:00', '14:30'), (4, '14:40', '16:10'), (5, '16:20', '17:50'), (6, '18:00', '19:30')
+insert into dbo.t_Lesson_Type (Name, Symbol) values ('Лекция', 'лк'), ('Практика','пр'), ('Лабораторная','лаб')
+
+
